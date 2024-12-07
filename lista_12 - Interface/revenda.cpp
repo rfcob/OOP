b76a66467fc04ccf956570059168198263d191eb
RF Cobo <107076651+rfcob@users.noreply.github.com>
2024-12-07 11:02:15 -0300

    Add files via upload

---
+++ lista_12 - Interface/revenda.cpp
@@ -0,0 +1,27 @@
+#include "cadastro_veiculos.hpp"
+
+int cadastro_veiculos::num_carros = 0;
+
+void cadastro_veiculos::init(string marca, string modelo, string combustivel, float valor, float km_rodados) {
+    this->marca = marca;
+    this->modelo = modelo;
+    this->combustivel = combustivel;
+    this->valor = valor;
+    this->km_rodados = km_rodados;
+    num_carros++;
+}
+
+void cadastro_veiculos::count_carros() {
+    num_carros++;
+}
+
+int cadastro_veiculos::num_count_carros() {
+    return num_carros;
+}
+
+string cadastro_veiculos::get_marca() { return marca; }
+string cadastro_veiculos::get_modelo() { return modelo; }
+string cadastro_veiculos::get_combustivel() { return combustivel; }
+float cadastro_veiculos::get_valor() { return valor; }
+float cadastro_veiculos::get_km_rodados() { return km_rodados; }
+
