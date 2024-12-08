64ec4e84f078841c003880a39f14c15c4490bbc2
RFcobo <rafaelcobo@outlook.com>
2024-12-08 11:08:04 -0300

    ;.

--- Lista_12 - Interface/cadastro_veiculos.hpp
+++ Lista_12 - Interface/cadastro_veiculos.hpp
@@ -5,21 +5,23 @@
 using namespace std;
 
 class cadastro_veiculos {
-private:
-    string marca, modelo, combustivel;
-    float valor, km_rodados;
-    static int num_carros;
 
-public:
-    void init(string marca, string modelo, string combustivel, float valor, float km_rodados);
-    //static void count_carros();
-    static int num_count_carros();
-    void count_carros();
-    string get_marca();
-    string get_modelo();
-    string get_combustivel();
-    float get_valor();
-    float get_km_rodados();
+    private:
+    
+        string marca, modelo, combustivel;
+        float valor, km_rodados;
+        static int num_carros;
+
+    public:
+
+        void init(string marca, string modelo, string combustivel, float valor, float km_rodados);
+        static int num_count_carros();
+        void count_carros();
+        string get_marca();
+        string get_modelo();
+        string get_combustivel();
+        float get_valor();
+        float get_km_rodados();
 };
 
 int cadastro_veiculos::num_carros = 0;
--- Lista_12 - Interface/revenda.cpp
+++ Lista_12 - Interface/revenda.cpp
@@ -1,13 +1,13 @@
 #include "cadastro_veiculos.hpp"
 
 
-void cadastro_veiculos::init(string marca, string modelo, string combustivel, float valor, float km_rodados) {
+void cadastro_veiculos::init(string marca, string modelo, string combustivel, float valor, float km_rodados){
     this->marca = marca;
     this->modelo = modelo;
     this->combustivel = combustivel;
     this->valor = valor;
     this->km_rodados = km_rodados;
-    num_carros++;
+    this->num_carros++;
 }
 
 void cadastro_veiculos::count_carros() {
@@ -18,9 +18,19 @@ int cadastro_veiculos::num_count_carros() {
     return num_carros;
 }
 
-string cadastro_veiculos::get_marca() { return marca; }
-string cadastro_veiculos::get_modelo() { return modelo; }
-string cadastro_veiculos::get_combustivel() { return combustivel; }
-float cadastro_veiculos::get_valor() { return valor; }
-float cadastro_veiculos::get_km_rodados() { return km_rodados; }
+string cadastro_veiculos::get_marca(){ 
+    return marca; 
+}
+string cadastro_veiculos::get_modelo(){ 
+    return modelo; 
+}
+string cadastro_veiculos::get_combustivel(){
+    return combustivel; 
+}
+float cadastro_veiculos::get_valor(){ 
+    return valor;
+}
+float cadastro_veiculos::get_km_rodados(){
+    return km_rodados; 
+}
 
